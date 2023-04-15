25827920291414540780d2ab9c8d1aa74e7bb76f
jfernandesdev <jfernandes.dev@gmail.com>
2023-04-15 13:45:08 -0300

    CHORE: list of users in table with pagination and filter

--- src/app/components/user-listing/user-listing.component.ts
+++ src/app/components/user-listing/user-listing.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, Injectable, ViewChild } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
 
 @Component({
   selector: 'app-user-listing',
   templateUrl: './user-listing.component.html',
   styleUrls: ['./user-listing.component.scss']
 })
-export class UserListingComponent {
 
+export class UserListingComponent {
+  constructor(private service: AuthService) {
+    this.loadUser()
+  }
+
+  userList: any;
+  dataSource: any;
+  @ViewChild(MatPaginator) paginator !: MatPaginator;
+  @ViewChild(MatSort) sort !: MatSort;
+
+  loadUser() {
+    this.service.getAll().subscribe(res => {
+      this.userList = res;
+      this.dataSource = new MatTableDataSource(this.userList);
+      this.dataSource.paginator = this.paginator;
+      this.dataSource.sort = this.sort;
+    })
+  }
+
+  displayedColumns: string[] = ['id', 'name', 'email', 'role', 'status', 'action'];
+
+  updateUser(code: any) {
+
+  }
 }
